01bfea97c0970e0b314ae78b79ebd4231e772564
Squid3rd <88415664+Squid3rd@users.noreply.github.com>
2024-03-08 19:42:48 +0700

    change 2

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"AG6ktGFDzsMXamFTLVcQkR1ffYdYD7K/jXBt9TO/RxM=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"5IOMJvkJCRkKgavrY3eSTO/1TpvEmwnkv7OZXibLgl4=\"}"
