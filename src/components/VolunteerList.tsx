cbb2d463fef7673e43382d98f97e302aafc56fbb
Nontapat Jiewnod <88415664+Squid3rd@users.noreply.github.com>
2024-03-10 00:23:42 +0700

    Update VolunteerList.tsx

--- src/components/VolunteerList.tsx
+++ src/components/VolunteerList.tsx
@@ -1,20 +1,27 @@
 import Link from "next/link"
 import ImageSlider from "./ImageSlider"
 
-const VolunteerList = () =>{
+const VolunteerList = ({data1}: any) =>{
+  // const pa1 = "/detail/" + param1
+  const  data  = data1;
+  // console.log("Mocl", data)
     return(
         <>
-        <Link href="/detail">
+        {data.map((item: any, index: any) => (
+          <div key={index}>
+        <Link href={"/detail/"+item.id}>
           <div className='flex flex-col w-full'>
             <ImageSlider urls={["/volunteer_day.jpg"]}/>
             <h3 className='mt-4 font-medium text-sm text-gray-700'>
-              VolunteerName
+              {item.vol_name}
             </h3>
             <p className='mt-1 text-sm text-gray-500'>
-              detail
+            {item.vol_detail}
             </p>
           </div>
         </Link>
+          </div>
+        ))}
         </>
     )
 
