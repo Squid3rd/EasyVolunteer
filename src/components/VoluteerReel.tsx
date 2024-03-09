8b29397b071cb1ee58a5f30d82b3fd026f412342
Nontapat Jiewnod <88415664+Squid3rd@users.noreply.github.com>
2024-03-10 00:23:59 +0700

    Update VoluteerReel.tsx

--- src/components/VoluteerReel.tsx
+++ src/components/VoluteerReel.tsx
@@ -1,6 +1,34 @@
 import VolunteerList from "./VolunteerList"
 
 const VolunteerReel = () =>{
+
+    const mockdata1 = {
+      vol_name: "Volunteer",
+      vol_detail: "sdddddddd"
+    }
+    const mockdata2 = [
+      {
+        id: '1',
+        vol_name: "สอนหนังสือเด็กด้อยโอกาส",
+        vol_detail: "สอนหนังสือพื้นฐาน ..."
+      },
+      {
+        id: '2',
+        vol_name: "ทำความสะอาดชายหาด",
+        vol_detail: "เก็บขยะ พลาสติก ..."
+      },
+      {
+        id: '3',
+        vol_name: "ปลูกป่า",
+        vol_detail: "ปลูกต้นไม้ในพื้นที่ป่า..."
+      },
+      {
+        id: '4',
+        vol_name: "ดูแลผู้สูงอายุ",
+        vol_detail: "ผู้สูงอายุ พูดคุย เล่น..."
+      }
+    ];
+
     return(
         <section className='py-12'>
         <div className='md:flex md:items-center md:justify-between mb-4'>
@@ -15,10 +43,7 @@ const VolunteerReel = () =>{
           <div className='mt-6 flex items-center w-full'>
             <div className='w-full grid grid-cols-2 gap-x-4 gap-y-10 sm:gap-x-6 md:grid-cols-4 md:gap-y-10 lg:gap-x-8'>
            
-              <VolunteerList/>
-              <VolunteerList/>
-              <VolunteerList/>
-              <VolunteerList/>
+              <VolunteerList data1={mockdata2} />
               {/* {map.map((product, i) => (
                 <ProductListing
                   key={`product-${i}`}
